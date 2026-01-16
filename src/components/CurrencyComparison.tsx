6f702c2bddd2d246caedd61b518bb996bd370b73
HamzaSlysol <hamza.slysol@gmail.com>
2026-01-16 17:15:25 +0500

    fix: console.log removed

--- src/components/CurrencyComparison.tsx
+++ src/components/CurrencyComparison.tsx
@@ -81,8 +81,6 @@ const Rates = () => {
     );
   };
 
-  console.log("Country code for flag:", selectedCountries[0]?.code);
-
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100">
       <div className="container mx-auto px-4 py-8 max-w-[1800px]">
